0994bfd42152a6bb06576eb7c2674ddeb3bf01bb
이지민 <wlals060330@gmail.com>
2023-03-22 10:55:02 +0900

    :memo: :: scheduler

--- src/main/java/dodam/b1nd/dgit/global/lib/scheduler/Scheduler.java
+++ src/main/java/dodam/b1nd/dgit/global/lib/scheduler/Scheduler.java
@@ -3,9 +3,11 @@ package dodam.b1nd.dgit.global.lib.scheduler;
 import dodam.b1nd.dgit.domain.github.service.GithubUserService;
 import github.query.GetUserQuery;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class Scheduler {
@@ -20,8 +22,7 @@ public class Scheduler {
                 GetUserQuery.User githubData = githubUserService.getData(githubUser.getGithubId()).getData().user();
                 githubUserService.update(githubData);
             } catch (Exception e) {
-                e.printStackTrace();
-                System.out.println(githubUser.getUser().getName() + " 업데이트 중 오류 발생");
+                log.error("fail to process file", e);
             }
         });
     }
