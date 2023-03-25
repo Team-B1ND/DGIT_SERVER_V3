5fdb9eae5346ef358e2c07543f8a4034c6e24e5c
이지민 <wlals060330@gmail.com>
2023-03-25 16:08:59 +0900

    :zap: :: n+1, select query decrease

--- src/main/java/dodam/b1nd/dgit/domain/github/repository/GithubRepositoryRepository.java
+++ src/main/java/dodam/b1nd/dgit/domain/github/repository/GithubRepositoryRepository.java
@@ -1,14 +1,17 @@
 package dodam.b1nd.dgit.domain.github.repository;
 
 import dodam.b1nd.dgit.domain.github.domain.entity.GithubRepository;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface GithubRepositoryRepository extends JpaRepository<GithubRepository, String> {
-
+public interface GithubRepositoryRepository extends JpaRepository<GithubRepository, Long> {
+    @EntityGraph(attributePaths = "repositoryOwner")
     List<GithubRepository> findAllByOrderByTotalStarsDesc();
 
+    @EntityGraph(attributePaths = "repositoryOwner")
+    List<GithubRepository> findAll();
 }
