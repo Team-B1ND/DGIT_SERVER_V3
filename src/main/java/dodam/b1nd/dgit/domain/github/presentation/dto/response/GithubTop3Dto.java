7f931f089fd8196d1ff20101191160582514e963
이지민 <wlals060330@gmail.com>
2023-05-24 23:29:46 +0900

    :sparkles: :: 주간 랭킹 TOP 3 기능 추가

---
+++ src/main/java/dodam/b1nd/dgit/domain/github/presentation/dto/response/GithubTop3Dto.java
@@ -0,0 +1,15 @@
+package dodam.b1nd.dgit.domain.github.presentation.dto.response;
+
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class GithubTop3Dto {
+    private String githubId;
+    private String name;
+    private int winCount;
+    private String userImage;
+    private String bio;
+}
